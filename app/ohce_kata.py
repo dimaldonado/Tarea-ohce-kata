ad1e8b3bd95516b30151a7c0322e5d3bb2d821ca
dimaldonado <102496218+dimaldonado@users.noreply.github.com>
2023-08-28 00:01:22 -0400

    unificacion de casos "noche", creacion caso base "dia"(test pasado)

--- app/ohce_kata.py
+++ app/ohce_kata.py
@@ -1,20 +1,9 @@
 def ohce_kata(hora):
-    if(hora==20):
+    #caso noche
+    if(20<=hora<=23):
         return "¡Buenas noches!"
-    if(hora==21):
+    if(0<=hora<6):
         return "¡Buenas noches!"
-    if(hora==22):
-        return "¡Buenas noches!"
-    if(hora==23):
-        return "¡Buenas noches!"
-    if(hora==1):
-        return "¡Buenas noches!"
-    if(hora==2):
-        return "¡Buenas noches!"
-    if(hora==3):
-        return "¡Buenas noches!"
-    if(hora==4):
-        return "¡Buenas noches!"
-    if(hora==5):
-        return "¡Buenas noches!"
-    
+    #caso dia
+    if(hora==6):
+        return "¡Buenos dias!"
