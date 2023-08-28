906bb8ccc24042685a2f4aecec3e0f0f4951beec
dimaldonado <102496218+dimaldonado@users.noreply.github.com>
2023-08-27 23:51:17 -0400

    caso buenas noches

--- app/ohce_kata.py
+++ app/ohce_kata.py
@@ -1,3 +1,20 @@
 def ohce_kata(hora):
     if(hora==20):
         return "¡Buenas noches!"
+    if(hora==21):
+        return "¡Buenas noches!"
+    if(hora==22):
+        return "¡Buenas noches!"
+    if(hora==23):
+        return "¡Buenas noches!"
+    if(hora==1):
+        return "¡Buenas noches!"
+    if(hora==2):
+        return "¡Buenas noches!"
+    if(hora==3):
+        return "¡Buenas noches!"
+    if(hora==4):
+        return "¡Buenas noches!"
+    if(hora==5):
+        return "¡Buenas noches!"
+    
--- test.py
+++ test.py
@@ -1,5 +1,15 @@
 import pytest
 from app.ohce_kata import ohce_kata
 
-def test_hora():
+def test_hora_noche():
     assert ohce_kata(20)=="¡Buenas noches!"
+    assert ohce_kata(21)=="¡Buenas noches!"
+    assert ohce_kata(22)=="¡Buenas noches!"
+    assert ohce_kata(23)=="¡Buenas noches!"
+    assert ohce_kata(1)=="¡Buenas noches!"
+    assert ohce_kata(2)=="¡Buenas noches!"
+    assert ohce_kata(3)=="¡Buenas noches!"
+    assert ohce_kata(4)=="¡Buenas noches!"
+    assert ohce_kata(5)=="¡Buenas noches!"
+
+    
