714d563f4349584f5f94c4ce83a6083706e40d30
dimaldonado <102496218+dimaldonado@users.noreply.github.com>
2023-08-29 17:23:53 -0400

    Update test.py
    Nuevo test (caso Stop)

--- test.py
+++ test.py
@@ -1,37 +1,37 @@
 import pytest
-from app.ohce_kata import ohce_kata
+from app.ohce_kata import ohce_kata_hora
 from app.ohce_kata import ohce_kata_palindromo
-
+from app.ohce_kata import ohce_kata_stop
 
 def test_hora_noche():
-    assert ohce_kata(20)=="¡Buenas noches!"
-    assert ohce_kata(21)=="¡Buenas noches!"
-    assert ohce_kata(22)=="¡Buenas noches!"
-    assert ohce_kata(23)=="¡Buenas noches!"
-    assert ohce_kata(1)=="¡Buenas noches!"
-    assert ohce_kata(2)=="¡Buenas noches!"
-    assert ohce_kata(3)=="¡Buenas noches!"
-    assert ohce_kata(4)=="¡Buenas noches!"
-    assert ohce_kata(5)=="¡Buenas noches!"
+    assert ohce_kata_hora(20)=="¡Buenas noches!"
+    assert ohce_kata_hora(21)=="¡Buenas noches!"
+    assert ohce_kata_hora(22)=="¡Buenas noches!"
+    assert ohce_kata_hora(23)=="¡Buenas noches!"
+    assert ohce_kata_hora(1)=="¡Buenas noches!"
+    assert ohce_kata_hora(2)=="¡Buenas noches!"
+    assert ohce_kata_hora(3)=="¡Buenas noches!"
+    assert ohce_kata_hora(4)=="¡Buenas noches!"
+    assert ohce_kata_hora(5)=="¡Buenas noches!"
 
     
 def test_hora_manana():
-    assert ohce_kata(6)=="¡Buenos dias!"
-    assert ohce_kata(7)=="¡Buenos dias!"
-    assert ohce_kata(8)=="¡Buenos dias!"
-    assert ohce_kata(9)=="¡Buenos dias!"
-    assert ohce_kata(10)=="¡Buenos dias!"
-    assert ohce_kata(11)=="¡Buenos dias!"
+    assert ohce_kata_hora(6)=="¡Buenos dias!"
+    assert ohce_kata_hora(7)=="¡Buenos dias!"
+    assert ohce_kata_hora(8)=="¡Buenos dias!"
+    assert ohce_kata_hora(9)=="¡Buenos dias!"
+    assert ohce_kata_hora(10)=="¡Buenos dias!"
+    assert ohce_kata_hora(11)=="¡Buenos dias!"
 
 def test_hora_tade():
-    assert ohce_kata(12)=="¡Buenas tardes!"
-    assert ohce_kata(13)=="¡Buenas tardes!"
-    assert ohce_kata(14)=="¡Buenas tardes!"
-    assert ohce_kata(15)=="¡Buenas tardes!"
-    assert ohce_kata(16)=="¡Buenas tardes!"
-    assert ohce_kata(17)=="¡Buenas tardes!"
-    assert ohce_kata(18)=="¡Buenas tardes!"
-    assert ohce_kata(19)=="¡Buenas tardes!"
+    assert ohce_kata_hora(12)=="¡Buenas tardes!"
+    assert ohce_kata_hora(13)=="¡Buenas tardes!"
+    assert ohce_kata_hora(14)=="¡Buenas tardes!"
+    assert ohce_kata_hora(15)=="¡Buenas tardes!"
+    assert ohce_kata_hora(16)=="¡Buenas tardes!"
+    assert ohce_kata_hora(17)=="¡Buenas tardes!"
+    assert ohce_kata_hora(18)=="¡Buenas tardes!"
+    assert ohce_kata_hora(19)=="¡Buenas tardes!"
 
 def test_palindromo():
     assert ohce_kata_palindromo("hola")=="aloh"
@@ -41,3 +41,7 @@ def test_palindromo():
     assert ohce_kata_palindromo("reconocer") == "¡Bonita palabra!"
     assert ohce_kata_palindromo("hola") == "aloh"
     assert ohce_kata_palindromo("12321") == "¡Bonita palabra!"
+
+def test_stop():
+    assert ohce_kata_stop("Stop!")==True
+    assert ohce_kata_stop("Stop!")==False
